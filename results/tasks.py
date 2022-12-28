1096d493869e1de417435780735fa38251e3102d
César Júnior <cesar.jnr@hotmail.com>
2022-12-27 21:15:37 -0300

    Start the web scrapping

--- results/tasks.py
+++ results/tasks.py
@@ -1,7 +1,22 @@
-from .celery import app
+import time
+import undetected_chromedriver.v2 as uc
+from selenium.webdriver.common.by import By
 
-@app.task
-def get_day_results():
-    print('Print hello world')
+# from .celery import app
 
-    return 'Hello World!'
+# @app.task
+# def get_day_results():
+# option = Options()
+# option.headless = True
+driver = uc.Chrome()
+driver.get('https://bet365.com')
+
+time.sleep(5)
+
+login_button = driver.find_element(By.CLASS_NAME, 'hm-MainHeaderRHSLoggedOutWide_LoginContainer')
+
+driver.execute_script('arguments[0].click();', login_button)
+
+time.sleep(5)
+
+driver.quit()
